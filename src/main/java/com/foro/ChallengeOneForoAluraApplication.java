6f38e9b7c9e2d830bdc6d776a24890261c3e4d4d
juankdns <juan.espinosa.dns@gmail.com>
2023-08-30 23:44:41 -0500

    feat: initial project setup

---
+++ src/main/java/com/foro/ChallengeOneForoAluraApplication.java
@@ -0,0 +1,13 @@
+package com.foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOneForoAluraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeOneForoAluraApplication.class, args);
+    }
+
+}
