a4d536129fcd86e8eee984c391bc7c073a40de82
juankdns <juan.espinosa.dns@gmail.com>
2023-08-31 22:04:02 -0500

    feat: add entities

---
+++ src/main/java/com/foro/domain/entity/CursoEntity.java
@@ -0,0 +1,26 @@
+package com.foro.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "curso")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CursoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String categoria;
+
+}
